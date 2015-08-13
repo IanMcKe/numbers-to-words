44fd3d4fd06971bbd166b7c9d1d4d86141d8286f
Ian McKenney & Ike Mattice <student@epicodus.com>
2015-08-13 14:59:52 -0700

    removed vendor files

--- src/NumbersToWords.php
+++ src/NumbersToWords.php
@@ -2,20 +2,36 @@
     class NumberToWords
     {
         function numberTransformer($input){
+            if ($input < 10) {
+                $digit_ones = array(
+                    0 => "zero",
+                    1 => "one",
+                    2 => "two",
+                    3 => "three",
+                    4 => "four",
+                    5 => "five",
+                    6 => "six",
+                    7 => "seven",
+                    8 => "eight",
+                    9 => "nine",
+                );
+                return $digit_ones[$input];
+            } else {
 
-            $digit_ones = array(
-                0 => "zero",
-                1 => "one",
-                2 => "two",
-                3 => "three",
-                4 => "four",
-                5 => "five",
-                6 => "six",
-                7 => "seven",
-                8 => "eight",
-                9 => "nine",
-            );
-            return $digit_ones[$input];
+                $digit_tens = array(
+                    10 => "ten",
+                    11 => "eleven",
+                    12 => "twelve",
+                    13 => "thirteen",
+                    14 => "fourteen",
+                    15 => "fifteen",
+                    16 => "sixteen",
+                    17 => "seventeen",
+                    18 => "eighteen",
+                    19 => "ninteen",
+                );
+                return $digit_tens[$input];
+            }
         }
     }
  ?>
